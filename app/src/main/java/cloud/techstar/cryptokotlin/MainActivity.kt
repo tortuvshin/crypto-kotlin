f337b57fd630f4a40955e414c0def1050c2007b8
tortuvshin <toroo.byamba@gmail.com>
2018-03-19 17:23:50 +0800

    load next ten coin

--- app/src/main/java/cloud/techstar/cryptokotlin/MainActivity.kt
+++ app/src/main/java/cloud/techstar/cryptokotlin/MainActivity.kt
@@ -2,8 +2,59 @@ package cloud.techstar.cryptokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Adapter
+import android.widget.Toast
+import cloud.techstar.cryptokotlin.`interface`.ILoadMore
+import cloud.techstar.cryptokotlin.adapters.CoinAdapter
+import cloud.techstar.cryptokotlin.models.Coins
+import cloud.techstar.cryptokotlin.utils.Utils
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ILoadMore {
+
+    internal var items:MutableList<Coins> = ArrayList()
+    internal lateinit var adapter: CoinAdapter
+    internal lateinit var client: OkHttpClient
+    internal lateinit var request: Request
+
+    override fun onLoadMore() {
+        if (items.size <= Utils.MAX_COIN_LOAD)
+            loadNext10Coin(items.size)
+        else
+            Toast.makeText(this@MainActivity, "Data max is"+Utils.MAX_COIN_LOAD, Toast.LENGTH_LONG).show()
+    }
+
+    private fun loadNext10Coin(index: Int){
+        client = OkHttpClient()
+        request = Request.Builder()
+                .url(String.format("https://api.coinmarketcap.com/v1/ticker/?start=%d&limit=10", index))
+                .build()
+
+        swife_to_refresh.isRefreshing = true
+        client.newCall(request)
+                .enqueue(object :Callback{
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        Log.d("ERROR: ", e.toString())
+                    }
+
+                    override fun onResponse(call: Call?, response: Response?) {
+                        val body = response.body()!!.string()
+                        val gson = Gson()
+                        val newItems = gson.fromJson<List<Coins>>(body, object:TypeToken<List<Coins>>(){}.type)
+                        runOnUiThread{
+                            items.addAll(newItems)
+                            adapter.setLoaded()
+                            adapter.updateData(items)
+                            swife_to_refresh.isRefreshing = false
+                        }
+                    }
+                })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
